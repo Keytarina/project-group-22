a960c1ea434f0ec6ee4358d7f5881c24275eefc6
Senriv <ikiry2013@gmail.com>
2023-02-20 21:26:16 +0200

    fix height modals and add open modals pages

--- src/main.js
+++ src/main.js
@@ -29,7 +29,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   var modalButtons = document.querySelectorAll('.js-open-modal'),
     overlay = document.querySelector('.js-overlay-modal'),
-    closeButtons = document.querySelectorAll('.js-modal-close');
+    closeButtons = document.querySelectorAll('.js-modal-close'),
+    body = document.querySelector('body');
 
   modalButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
@@ -41,6 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         );
       modalElem.classList.add('active');
       overlay.classList.add('active');
+      body.classList.add('no-scroll');
     });
   });
 
@@ -50,6 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       parentModal.classList.remove('active');
       overlay.classList.remove('active');
+      body.classList.remove('no-scroll');
     });
   });
 
@@ -61,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if (key == 27) {
         document.querySelector('.modal.active').classList.remove('active');
         document.querySelector('.overlay').classList.remove('active');
+        document.querySelector('body').classList.remove('no-scroll');
       }
     },
     false
@@ -69,6 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
   overlay.addEventListener('click', function () {
     document.querySelector('.modal.active').classList.remove('active');
     this.classList.remove('active');
+    document.querySelector('body').classList.remove('no-scroll');
   });
 });
 
