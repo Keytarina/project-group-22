955b37ed8d50ea8bbc5827126017d9132385a0a8
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-02-22 16:36:27 +0200

    Fix error require

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+global.require = require;
 
 export default defineConfig({
   root: 'src',
