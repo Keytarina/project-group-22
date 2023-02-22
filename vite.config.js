6b2fdb9ae4ce70e0bf7b4724398ab3c9aee00248
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-02-22 15:55:16 +0200

    Fix require

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import './src/js/require';
+// import './src/js/require';
 
 export default defineConfig({
   root: 'src',
